8f558090d338d74771ba741600ad91c1eea43f8e
Manish <manishguhe301@gmail.com>
2025-03-12 20:07:57 +0530

    feat: added the loaders and assets

---
+++ components/elements/Loader.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className='container'>
+      <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
+    </div>
+  );
+};
+
+export default Loader;
