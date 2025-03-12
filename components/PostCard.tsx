e5589c3381a312f5cc1a35290cb18a4983c6ed0b
Manish <manishguhe301@gmail.com>
2025-03-12 20:26:40 +0530

    fix: minor fix

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,10 +1,17 @@
+'use client';
+import { useUser } from '@/utils/hooks/useUser';
 import { Post } from '@prisma/client';
 import Image from 'next/image';
 
 export const PostCard = ({ post }: { post: Post }) => {
+  const { user } = useUser();
   return (
     <div
-      className='p-[10px] rounded-[0.375rem] bg-[#fff] cursor-pointer border border-[#E2E2E2] hover:border-[#E8E8E8] transition-all duration-100 hover:bg-gray-50 flex items-center gap-2'
+      className={`p-[10px] rounded-[0.375rem] bg-[#fff]  border border-[#E2E2E2]  transition-all duration-100  flex items-center gap-2 ${
+        !user?.id
+          ? 'cursor-not-allowed'
+          : 'cursor-pointer hover:bg-gray-50 hover:border-[#E8E8E8]'
+      }`}
       key={post.id}
     >
       <div className='flex flex-col items-center justify-center gap-0.5 rounded-lg border border-[#DBDBDB] px-2 py-1.5  space-x-0 text-xs leading-3 h-[40px] w-[35px]'>
